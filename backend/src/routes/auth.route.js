298bb4caa599538e20458eb620eef7f0a39e8533
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2025-11-04 02:56:11 +0500

    resolve some issues

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,7 +3,7 @@ import {  login, logout, Signup } from "../controllers/auth.controller.js"
 
 const router = express.Router()
 
-router.post("/signup", Signup)
+router.post("/signup",  Signup)
 router.post("/login", login)
 router.post("/logout", logout)
 
