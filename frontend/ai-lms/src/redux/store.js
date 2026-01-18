35099748f0d02cfdef1ab53869e1eab829bac0f1
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2026-01-19 02:16:32 +0500

    add enrollcourses page

--- frontend/ai-lms/src/redux/store.js
+++ frontend/ai-lms/src/redux/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // uses localStorage
+import userReducer from './userSlice';
 
-import userSlice from "./userSlice";
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
-    reducer:{
-        user: userSlice
-        
-    }
-})
+  reducer: {
+    user: persistedReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
