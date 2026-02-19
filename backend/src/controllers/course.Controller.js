a6c060ed90bdaacf891dcab3c0a0d8519265ac27
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2026-02-20 02:41:55 +0500

    set view courses

--- backend/src/controllers/course.Controller.js
+++ backend/src/controllers/course.Controller.js
@@ -42,7 +42,7 @@ export const createCourse = async (req, res) => {
 
 export const getPublishedCourses = async (req, res) => {
   try {
-    const courses = await Course.find({ isPublished: true });
+    const courses = await Course.find({ isPublished: true }).populate("lectures");
     if (!courses || courses.length === 0) {
       return res.status(404).json({
         success: false,
